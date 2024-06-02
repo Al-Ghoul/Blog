38bec10ff1072397938d25172b21934b94681e37
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-06-02 08:03:39 +0300

    feat(initial): add inital blog structure/files

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+export default defineConfig({
+  site: "https://alghoul.dev",
+  integrations: [mdx(), sitemap(), tailwind()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["ar", "en"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+  markdown: {
+    remarkPlugins: [
+      remarkMath,
+    ],
+    rehypePlugins: [
+      // This function MUST be an async
+      // deno-lint-ignore require-await
+      () => async (tree) => {
+        for (const node of tree.children) {
+          if (
+            node.tagName === "pre" && node.properties &&
+            node.properties.className
+          ) {
+            node.properties.className.push("mockup-code");
+          }
+          if (node.tagName === "blockquote" && node.properties) {
+            node.properties.className = ["alert"];
+          }
+        }
+      },
+      rehypeKatex,
+    ],
+  },
+});
---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "AlGhoul's Blog";
+export const SITE_DESCRIPTION =
+  "A developer's blog that's meant to be creative, inspirational & educational.";
+export const TRANSITION_API = true;
